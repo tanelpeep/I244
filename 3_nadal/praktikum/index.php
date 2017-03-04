d49dd03669f2135b5f7a31d0a13c1841775130f7
Tanel Peep <tpeep@itcollege.ee>
2017-03-04 13:15:10 +0200

    praktikum

---
+++ 3_nadal/praktikum/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <script>
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+</script>
+  </script>
+  <meta charset="UTF-8"/>
+  <title> 
+  Teretulemast!
+  </title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+ </head>
+ 
+ <body onload="startTime()">
+	<h2 id="pealkiri">Tere, mina olen Tanel.</h2>
+	<img src="pics/ws83871f77-cec0-4f5c-a99f-58a58476fb19_portrait.jpg" alt="Mina">
+	<h2 id="txt" />
+	<h2>Ma näen sind: <?php echo $_SERVER['REMOTE_ADDR'];?></h2>
+	<p><?php echo 'Kasutatav PHP versioon: ' .phpversion();?><p/>
+	
+	<?php 
+    $host = "localhost";
+    $user = "test";
+    $pass = "t3st3r123";
+    $db = "test";
+
+	$conn = new mysqli($host, $user, $pass, $db);
+	
+	if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+	} 
+
+	$sql = "SELECT kasutajaID, kasutajanimi FROM 0001kasutajad";
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+		echo "<table><tr><th>ID</th><th>Kasutajanimi</th></tr>";
+		// output data of each row
+		while($row = $result->fetch_assoc()) {
+			echo "<tr><td>" . $row["kasutajaID"]. "</td><td>" . $row["kasutajanimi"]. "</td></tr>";
+		}
+		    echo "</table>";
+			
+	} else {
+		echo "0 results";
+	}
+	$conn->close();
+	?>
+	
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse feugiat libero id mauris tempor, faucibus finibus risus gravida. Morbi id ex urna. Morbi vitae metus eget justo vulputate tempor et vitae libero. Cras leo mauris, semper sed mauris ac, rutrum viverra lectus. Ut eu lorem vel felis hendrerit fermentum et sit amet enim. Vestibulum tempus turpis est, nec pretium tellus porttitor ut. Praesent vel justo ut erat dignissim varius. Maecenas imperdiet velit vitae massa hendrerit rutrum. Cras maximus scelerisque efficitur. Quisque laoreet posuere orci. Etiam facilisis tempus lacinia.</p>
+	<p>Duis tristique libero sit amet massa iaculis, non dignissim dui semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean ultricies consequat felis blandit tempus. Quisque risus metus, venenatis vel tellus vel, egestas facilisis augue. Phasellus ipsum elit, facilisis quis neque et, ultrices luctus tortor. Duis et libero convallis, ornare lacus sed, pellentesque justo. Quisque urna augue, mattis ac faucibus et, commodo vel nisi. Ut dapibus risus et nisl luctus, ac iaculis ex molestie. In dui odio, auctor sed imperdiet et, porta vel nulla. In sodales est dui, non euismod elit pharetra id.</p>
+	<p>Maecenas imperdiet suscipit eros. Sed augue urna, iaculis nec ante a, euismod pretium orci. Donec ultricies congue neque, a dictum arcu molestie in. Nulla eget iaculis libero. Integer porttitor velit malesuada massa placerat elementum. Mauris ultricies varius orci. Donec vel est quis tellus mattis feugiat vitae lobortis orci. Fusce viverra sapien leo, condimentum blandit nibh imperdiet a. Nullam quis magna laoreet, volutpat ex sit amet, semper nunc. Proin hendrerit ultrices dignissim. Vivamus congue eros lacus, in fringilla massa consectetur a. Nunc non lacus orci. Duis ultricies dui magna, eget volutpat nisl scelerisque non. Nullam aliquam dictum dictum. In ut mi in dui sollicitudin mattis eleifend vitae nisi. Aliquam lobortis purus sed eros cursus sollicitudin.</p>
+ </body>
+</html>
