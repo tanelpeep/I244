f0a1e02011564ac5fb09bfd0f4fc84f6ecbbfc10
Tanel Peep <tanel.peep@outlook.com>
2017-03-12 13:39:34 +0200

    4. praktikum - CSS validation

--- 4_nadal/praktikum/index.php
+++ 4_nadal/praktikum/index.php
@@ -95,8 +95,8 @@ function checkTime(i) {
  <a href="http://validator.w3.org/check?uri=http://enos.itcollege.ee/~tpeep/i244/3_nadal/praktikum/index.php">
   <img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
  </a>
- <a href="http://jigsaw.w3.org/css-validator/check/http://enos.itcollege.ee/~tpeep/i244/3_nadal/praktikum/index.php">
-  <img src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!" />
+ <a href="http://jigsaw.w3.org/css-validator/check/http://enos.itcollege.ee/~tpeep/i244/3_nadal/praktikum/stiil.css">
+  <img src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="Valid CSS!" />
 </a>
 </p>
  </div>
