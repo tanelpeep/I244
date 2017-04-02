57e2fae06446e5e42b3c23c9a287984d1b39c963
Tanel Peep <tanel.peep@outlook.com>
2017-04-02 13:27:56 +0300

    7. nädal - 3 ülesanne

--- 7_nadal/ylesanne/index.php
+++ 7_nadal/ylesanne/index.php
@@ -21,6 +21,13 @@
     width: 800px;
     margin: auto;
 	}
+	.kass {
+	border: 1px solid black;
+	border-radius: 5px;
+	display: inline-block;
+	margin: 1%;
+	padding: 3px;
+	}
     </style>
 </head>
 <body>
@@ -46,10 +53,6 @@
 $text="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
 At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat
 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-
-		/*
-			SIIN ESITADA TEKST NII; ET IGA PRAGRAHVI ALGUSTÄHT ON SUUR (span vahel)
-		*/
 		
 		$p = explode(PHP_EOL, $text);
 		//echo $p[2];
@@ -61,6 +64,15 @@ Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
 		?>
 		
 		<h1>Infomassiiv include abil</h1>
+		
+		<?php 
+		include 'massiiv.php';
+		foreach ($kassid as $kass)
+		{
+			echo "<p class='kass'><b>".$kass['nimi']."</b> on ".$kass['varvus']. ", ".$kass['vanus']." aastane kass, kellele ".$kass['omadus'].".</p>";
+		}
+		
+		?>
 		
 	</div>
 </body>
--- 7_nadal/ylesanne/massiiv.php
+++ 7_nadal/ylesanne/massiiv.php
@@ -1,6 +1,9 @@
 <?php
 $kassid= array( 
-		array('nimi'=>'Miisu', 'vanus'=>2), 
-		array('nimi'=>'Tom', 'vanus'=>1)
+		array('nimi'=>'Miisu', 'vanus'=>2, 'varvus'=>'kollane', 'omadus'=>'ei tasu lähedale minna'), 
+		array('nimi'=>'Nurr', 'vanus'=>7, 'varvus'=>'must-valge', 'omadus'=>'meeldib pahandusi teha'), 
+		array('nimi'=>'Sofia', 'vanus'=>3, 'varvus'=>'hall', 'omadus'=>'meeldib asju ümber ajada'), 
+		array('nimi'=>'Tom', 'vanus'=>1, 'varvus'=>'must', 'omadus'=>'meeldivad lapsed'), 
+		array('nimi'=>'Peeter', 'vanus'=>4, 'varvus'=>'laiguline', 'omadus'=>'on väga raske sobivat toitu leida')
 	);
 ?>
