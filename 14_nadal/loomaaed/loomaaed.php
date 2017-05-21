01aed8eb8309198a752fd08e75049a3f721345ec
Tanel Peep <tanel.peep@outlook.com>
2017-05-21 22:02:18 +0300

    14. nädal - Turvalisus

---
+++ 14_nadal/loomaaed/loomaaed.php
@@ -0,0 +1,42 @@
+<?php
+require_once('funk.php');
+session_start();
+connect_db();
+
+$page="pealeht";
+if (isset($_GET['page']) && $_GET['page']!=""){
+	$page=htmlspecialchars($_GET['page']);
+}
+
+
+include_once('views/head.html');
+
+switch($page){
+	case "login":
+		logi();
+	break;
+	case "loomad":
+		kuva_puurid();
+	break;
+	case "logout":
+		logout();
+	break;
+	case "lisa":
+		lisa();
+	break;
+    case "muuda":
+        muuda();
+        break;
+	default:
+        if (isset($_SESSION['user']) && $_SESSION['roll'] == 'admin'){
+            header("Location: loomaaed.php?page=lisa");
+        }
+        else {
+            include_once('views/v2rav.html');
+        }
+	break;
+}
+
+include_once('views/foot.html');
+
+?>
